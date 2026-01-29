83e54ce476c46647a555a93038fc3e6b9723938b
MunTech Operations <operations.muntech@gmail.com>
2026-01-29 22:24:59 +0300

    Fix Django model clash: Add related_name to User groups and permissions

--- backend/users/models.py
+++ backend/users/models.py
@@ -13,5 +13,8 @@ class User(AbstractUser):
     last_sync = models.DateTimeField(null=True, blank=True)
     is_verified = models.BooleanField(default=False)
     
+    groups = models.ManyToManyField('auth.Group', related_name='custom_user_set', blank=True, help_text='The groups this user belongs to.')
+    user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_set_perm', blank=True, help_text='Specific permissions for this user.')
+    
     def __str__(self):
         return self.username
